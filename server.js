b23cae0973b12171f3b3b7fc4fcb3b4d0822172d
somyabansal25 <156185594+somyabansal25@users.noreply.github.com>
2024-11-04 14:15:49 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
